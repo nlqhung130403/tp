fd53c549d06e97c76d3a1812eac8ce29a4bf75cc
nlqhung130403 <e1157278@u.nus.edu>
2025-03-13 04:39:41 +0800

    Add new productPrefence and frequency to Client

---
+++ src/main/java/seedu/address/model/client/Frequency.java
@@ -0,0 +1,39 @@
+package seedu.address.model.client;
+
+public class Frequency {
+    public static final String MESSAGE_CONSTRAINTS = "Frequency should be a non-negative integer";
+
+    public final int frequency;
+
+    public Frequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public static boolean isValidFrequency(int frequency) {
+        return frequency >= 0;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(frequency);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Frequency)) {
+            return false;
+        }
+
+        Frequency otherFrequency = (Frequency) other;
+        return frequency == otherFrequency.frequency;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(frequency);
+    }
+}
---
+++ src/main/java/seedu/address/model/client/ProductPreference.java
@@ -0,0 +1,34 @@
+package seedu.address.model.client;
+
+public class ProductPreference {
+    public final String productPreference;
+
+    public ProductPreference(String productPreference) {
+        this.productPreference = productPreference;
+    }
+
+    @Override
+    public String toString() {
+        return productPreference;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof ProductPreference)) {
+            return false;
+        }
+
+        ProductPreference otherProductPreference = (ProductPreference) other;
+        return productPreference.trim().equalsIgnoreCase(otherProductPreference.productPreference.trim());
+    }
+
+    @Override
+    public int hashCode() {
+        return productPreference.hashCode();
+    }
+
+}
