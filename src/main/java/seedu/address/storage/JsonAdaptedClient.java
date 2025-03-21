e44aabb81db210c1673ac3ce4ebc9ea33248f163
LAPTOP-OR4J58MA\TANG YUKUN <e1297789@u.nus.edu>
2025-03-21 23:47:27 +0800

    fix the bug that when pref is null, the ui still shows

--- src/main/java/seedu/address/storage/JsonAdaptedClient.java
+++ src/main/java/seedu/address/storage/JsonAdaptedClient.java
@@ -1,10 +1,6 @@
 package seedu.address.storage;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -51,7 +47,7 @@ class JsonAdaptedClient {
         if (tags != null) {
             this.tags.addAll(tags);
         }
-        this.productPreference = productPreference;
+        this.productPreference = Objects.requireNonNullElse(productPreference, "");
         this.totalPurchase = totalPurchase;
     }
 
@@ -116,21 +112,20 @@ class JsonAdaptedClient {
 
         final Set<Tag> modelTags = new HashSet<>(clientTags);
 
-
-        if (productPreference == null) {
-            return new Client(modelName, modelPhone, modelEmail, modelAddress, modelTags,
-                    Optional.ofNullable(null));
-        }
-
         if (!Frequency.isValidFrequency(totalPurchase)) {
             throw new IllegalValueException(Frequency.MESSAGE_CONSTRAINTS);
         }
         final Frequency productFrequency = new Frequency(totalPurchase);
 
-        final ProductPreference modelProductPreference = new ProductPreference(productPreference, productFrequency);
+        final Optional<ProductPreference> modelProductPreference;
+        if (productPreference.isEmpty()) {
+            modelProductPreference = Optional.empty();
+        } else {
+            modelProductPreference = Optional.of(new ProductPreference(productPreference, productFrequency));
+        }
 
         return new Client(modelName, modelPhone, modelEmail, modelAddress, modelTags,
-                Optional.of(modelProductPreference));
+                modelProductPreference);
     }
 
 }
