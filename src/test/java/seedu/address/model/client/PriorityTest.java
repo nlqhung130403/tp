2b41f7722d227fc1f962c5ab4694ce440c99b167
nlqhung130403 <e1157278@u.nus.edu>
2025-03-27 22:15:16 +0800

    Fix styles

--- src/test/java/seedu/address/model/client/PriorityTest.java
+++ src/test/java/seedu/address/model/client/PriorityTest.java
@@ -1,30 +1,31 @@
 package seedu.address.model.client;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.commons.exceptions.IllegalValueException;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
 
 public class PriorityTest {
 
     @Test
-    public void fromIntTest_validInt_successful(){
+    public void fromIntTest_validInt_successful() {
         assertEquals(Priority.fromInt(1), Priority.STANDARD);
         assertEquals(Priority.fromInt(2), Priority.PREMIUM);
         assertEquals(Priority.fromInt(3), Priority.VIP);
     }
 
     @Test
-    public void fromIntTest_invalidInt_throwsIllegalArgumentException(){
+    public void fromIntTest_invalidInt_throwsIllegalArgumentException() {
         assertThrows(IllegalArgumentException.class, () -> Priority.fromInt(0));
         assertThrows(IllegalArgumentException.class, () -> Priority.fromInt(4));
     }
 
     @Test
-    public void toStringTest(){
+    public void toStringTest() {
         assertEquals(Priority.STANDARD.toString(), "STANDARD");
         assertEquals(Priority.PREMIUM.toString(), "PREMIUM");
         assertEquals(Priority.VIP.toString(), "VIP");
